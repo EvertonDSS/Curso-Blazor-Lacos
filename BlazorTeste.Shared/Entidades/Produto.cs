a00eea59c912f7c23e60cb85b5086382db4b8ca1
evert <evert@EVERTON>
2023-01-23 17:08:16 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorTeste.Shared/Entidades/Produto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTeste.Shared.Entidades {
+    public class Produto {
+        public string Nome { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
